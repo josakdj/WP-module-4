ff63ff4a5e01e4773fa59cf51e2a2eaf96cdc387
josakdj <amararifi1234@gmail.com>
2025-11-10 18:30:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,13 @@
-<?php 
-
-
-get_header();
-
-?>
-
+<?php get_header(); ?>
+<main>
 <?php
 if (have_posts()) : 
   while (have_posts()): the_post(); ?>
-
-
-
-    <h1><?php the_title(); ?></h1>
+  <h1><?php the_title(); ?></h1>
 <small>Posted on: <?php the_time('F , j , Y') ?> at <?php the_time('g:i a') ?> in <?php the_category(', '); ?>
     <?php the_content(); ?>
     <?php endwhile;
     endif;?>
     </small>
-
-
-<?php 
-
-
-get_footer();
-
-?>
+  </main>
+<?php get_footer(); ?>
